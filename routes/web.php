3d8e9ba0569b3ca7b663d97f6168958bd258882c
Lin Htet Paing <linhtetpaing9@gmail.com>
2018-04-06 23:23:03 +0630

    DataBase SetUp

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/abouts', function () {
+    return view('abouts');
 });
